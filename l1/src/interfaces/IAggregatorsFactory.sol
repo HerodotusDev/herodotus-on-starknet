d22cb1c502dfc46845933e01e5471e561e9ef007
Filip Krawczyk <kotmawszy@gmail.com>
2024-06-07 18:30:25 +0200

    Remove L1 directory

--- l1/src/interfaces/IAggregatorsFactory.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.21;
-
-import {IAggregator} from "./IAggregator.sol";
-
-interface IAggregatorsFactory {
-    function aggregatorsById(uint256 id) external view returns (address);
-}
